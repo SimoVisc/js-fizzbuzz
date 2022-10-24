19b8184fc08cef1317060eddb8541410f3accca6
SimoVisc <simonaviscardivs@gmail.com>
2022-10-24 16:07:21 +0200

    aggiunge la parola Fizz ai multipli di 3, ma c'è un errore

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ const ul = document.getElementById("numbers");
 for(let i = 1; i<=100; i++){
     const element = `<li>${i}</li>`;
     ul.innerHTML += element;
+    if(i % 3 ===0){
+        console.log(i)= "Fizz"
+    }
 }
+console.log(element,)
+
+
+
